79b73279a38d7e9aa6991d5766013a1bf6b042b6
Yohith-Programmer101 <yohithprogrammer101@gmail.com>
2021-08-17 20:07:13 +0530

    Finish Push button project

---
+++ LED Light Up Push Button/LED Light Up Push Button.py
@@ -0,0 +1,19 @@
+print('''
+First open the "Connections - 1.png" and connect the ciruit as it is in the image.
+Initially the led will be off, when you press the button the led will light up.
+Circuits like this can run without a computer or a microcontroller.
+So you can try replacing the arduino board with a 9v battery or view the "Connectios - A1.png".
+''')
+print("="*100)
+print('''
+Secondly open the "Connections - 2.png" and connect the circuit as it is in the image.
+Initially the led will be light up, when you press the push button the led will turn off.
+Circuits like this can run without a computer or a microcontroller.
+So you can try replacing the arduino board with a 9v battery or view the "Connections - A2.png".
+''')
+print("="*100)
+print('''
+To see the Circuit Diagram of the mini push button view the "Circuit Diagram.jpg" image.
+It will give you a brief understanding of the circuit.
+''')
+print("="*100)
